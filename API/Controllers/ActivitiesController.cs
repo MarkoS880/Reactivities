a68fa20fc248a9bc6d71bb1f4516028905cba65b
unknown <stankovicm0808@gmail.com>
2024-09-24 19:09:35 +0200

    End of CRUD section

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,16 +19,35 @@ namespace API.Controllers
         [HttpGet("{id}")] //api/activities/someActiviti
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query { Id = id});
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
-        { 
+        public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+        {
             await Mediator.Send(new Create.Command { Activity = activity });
 
             return Ok();
         }
 
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+
+            await Mediator.Send(new Edit.Command { Activity = activity });
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            await Mediator.Send(new Delete.Command { Id = id });
+
+            return Ok();
+        }
     }
 }
