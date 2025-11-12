4c3acc3fe488f1b5f6f6b7473724322bdee5af4d
mastankovic <mastankovic@mobibanka.rs>
2025-11-12 13:55:57 +0100

    End of section 10

---
+++ Application/Activities/DTOs/EditActivityDto.cs
@@ -0,0 +1,15 @@
+﻿using MediatR.NotificationPublishers;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities.DTOs
+{
+    public class EditActivityDto : BaseActivityDto
+    {
+        public string Id { get; set; } = "";
+    }
+}
